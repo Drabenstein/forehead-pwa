604dc1d1d880977b87879760ce8fead45373298d
Marcin Drabek <marcindrabek@hotmail.com>
2023-07-01 20:09:16 +0200

    Fixed useCountdown performance and changed api endpoint

--- src/api/questionApi.ts
+++ src/api/questionApi.ts
@@ -1,13 +1,8 @@
-const apiUrl = "http://localhost:3000/api";
+const apiUrl = "http://localhost:5194/api";
 
 const questionApi = {
   getQuestionsForCategory: async (categoryId: number) => {
-    return await fetch(
-      `${apiUrl}/questions?` +
-        new URLSearchParams({
-          categoryId: categoryId.toString(),
-        })
-    )
+    return await fetch(`${apiUrl}/categories/${categoryId}/questions`)
       .then((response) => response.json())
       .catch((error) => {
         console.log(error);
--- src/hooks/useCountdown.tsx
+++ src/hooks/useCountdown.tsx
@@ -1,16 +1,22 @@
 import { MutableRefObject, useEffect, useRef, useState } from "react";
 
 const useCountdown = (seconds: number) => {
+  const currentDate = new Date();
+  const [desiredDate] = useState(new Date(currentDate.getTime() + seconds * 1000));
   const [countDown, setCountDown] = useState(seconds * 1000);
 
   const intervalRef: MutableRefObject<NodeJS.Timer | undefined> = useRef();
 
-  useEffect(() => {
-    intervalRef.current = setInterval(() => {
-      setCountDown((prev: number) => (prev > 0 ? prev - 100 : 0));
-    }, 100);
+  const countdownWork = () => {
+      const now = new Date();
+      setCountDown(desiredDate.getTime() - now.getTime());
+      intervalRef.current = setTimeout(countdownWork, 100);
+  };
 
-    return () => clearInterval(intervalRef.current);
+  useEffect(() => {
+    intervalRef.current = setTimeout(countdownWork, 100);
+
+    return () => clearTimeout(intervalRef.current);
   }, [seconds]);
 
   return countDown;
